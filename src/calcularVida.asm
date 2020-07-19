47759c8a620ce164f03054aeda3ac2e7b33e3f9a
nicolsss <nicole.gq@hotmail.com>
2020-07-18 21:17:08 -0500

    Modificacion de funciones calcularVida y compareStrings

--- src/calcularVida.asm
+++ src/calcularVida.asm
@@ -2,46 +2,30 @@
 .text
 .globl calcularVida
 
-#Retorna la vida restante del pokemon, recibe en a0 la vida el contrincante y en a1 el multiplicador del ataque	
-#----------------------------------------------Funcion calcular vida-------------------------------------------------	
-calcularVida:
-	addi $sp,$sp,-20
-	sw   $s0,0($sp)
-	sw   $s1,4($sp)
-	sw   $t0,8($sp)
-	sw   $t1,16($sp)
-	sw   $ra,20($sp)
+#recibe en a0 ataque de p1, y a1 vida de p2. Retorna nueva vida de p2
+#-------------------------------------Calcular vida de p2------------------------------------------------------------
 	
+calcularVida:	
+	addi $sp,$sp,-4
+	sw $t0,0($sp)
+	sw $ra,4($sp)
 	
-	li $s0,2		#ataque
-	li $s1,3		#codigo mutiplicador 1/2
-	
-	beq $a1,$s1,division
-	mul $t0,$s0,$a1		#t0=2*multiplicador
-	j restarVida
-	
-division:
-	srl $t0,$s0,1
-	
-restarVida:
-	slt $t1,$a0,$t0
-	beq $t1,$zero,restar
-	add $v0,$zero,$zero
+
+
+	slt $t0,$a1,$a0
+	bne $t0,$zero,lost
+	sub $v0,$a1,$a0	
 	j returnVida
 	
-			
-
-restar:
-	sub $v0,$a0,$t0
-
-returnVida:
+		
+lost:
+	add $v0,$zero,$zero
 	
-	sw   $s0,0($sp)
-	sw   $s1,4($sp)
-	sw   $t0,8($sp)
-	sw   $t1,16($sp)
-	sw   $ra,20($sp)
-	addi $sp,$sp,20
+returnVida:
+	lw $t0,0($sp)
+	lw $ra,4($sp)
+	addi $sp,$sp,4
 	
 	jr $ra
-#--------------------------------------------------------------------------------------------------------------------	
+	
+#-----------------------------------------------------------------------------------------------------------------------
--- src/compareStrings.asm
+++ src/compareStrings.asm
@@ -4,27 +4,30 @@
 
 
 #comparar strings retorna 1 si son iguales, 0 sino
+#a0 es el primer string,a1 es el segundo string y a2 el len del primer string
 #------------------------------------------------------Funcion comparar strings ---------------------------------
 	
 compareStrings:
-	addi $sp,$sp,-16
+	addi $sp,$sp,-20
 	sw $a0,0($sp)
 	sw $a1,4($sp)
 	sw $t2,8($sp)
 	sw $t3,12($sp)
 	sw $ra,16($sp)
+	sw $a2,20($sp)
 	
+	add $t0,$zero,$zero		#contador
 
-
-comparar:	
+comparar:
+	beq $t0,$a2,correcto
 	lb $t2,0($a0)		#caracter de t2-->str1
 	lb $t3,0($a1)		#caracter de t3-->str2
 	
-	beq $t2,$zero,correcto	#Termina de recorrer el string
 	
 	bne  $t2,$t3,incorrecto	#comparar si dos caracteres son diferentes
 	addi  $a0,$a0,1
 	addi  $a1,$a1,1
+	addi  $t0,$t0,1		#Aumento el contador
 	b comparar
 	
 	
@@ -44,7 +47,8 @@ returnCompareString:
 	lw $t2,8($sp)
 	lw $t3,12($sp)
 	lw $ra,16($sp)
-	addi $sp,$sp,16
+	lw $a2,20($sp)
+	addi $sp,$sp,20
 	
 	jr $ra
 #-----------------------------------------------------------------
